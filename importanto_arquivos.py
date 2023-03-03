e0a70a2f5c53c9971527f589b453ad96e94707bc
Cinthia Kazue Arizono <carizono@gmail.com>
2023-03-02 23:29:32 -0300

    Criando fun;'ao importar e converter pdf

--- importanto_arquivos.py
+++ importanto_arquivos.py
@@ -1,3 +1,15 @@
+import pandas as pd
+import tabula
 
+url_excel = 'https://docs.google.com/spreadsheets/d/1HPlw9FPQ3qkHmZMziGbME7aqiPnZs08bjoFuJbN07NI/edit#gid=0'
 url_pdf = 'https://www.fitabase.com/media/1930/fitabasedatadictionary102320.pdf'
 url_txt = 'https://www.kaggle.com/kenmatsu4/feature-store-for-indoor-location-navigation'
+def arquivoPdf(arquivo):
+
+    #importando o arquivo pdf para o python
+    pdf = tabula.read_pdf(url_pdf, pages = 'all')
+    #convertendo o arquivo pdf em csv
+    tabula.convert_into(url_pdf, 'pdf.csv', output_format = 'csv', pages = 'all')
+
+excel = pd.read_excel(url_excel)
+excel.to_csv(url_excel)
